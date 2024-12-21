0703427eba41a52cc82635fc3e1c8842446d7ba4
u8p <154946576+xap8Em@users.noreply.github.com>
2024-12-21 18:43:37 +0900

    Make State inner class of StateMachine

--- state_machine.gd
+++ state_machine.gd
@@ -54,3 +54,31 @@ func _change_current_state_to(next_state_name: StringName) -> void:
 	_current_state.enter()
 
 	current_state_changed.emit(_current_state.get_name())
+
+
+class State extends RefCounted:
+	signal exiting(next_state_name: StringName)
+
+
+	func enter() -> void:
+		pass
+
+
+	func input(_event: InputEvent) -> void:
+		pass
+
+
+	func unhandled_input(_event: InputEvent) -> void:
+		pass
+
+
+	func physics_process(_delta: float) -> void:
+		pass
+
+
+	func process(_delta: float) -> void:
+		pass
+
+
+	func exit() -> void:
+		pass
